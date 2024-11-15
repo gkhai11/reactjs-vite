ff6c4547a678d3eaed413f1c0b09a5a2ddc4f59f
pgkhai <pgkhai1121@gmail.com>
2024-11-15 12:05:55 +0700

    'react-08'

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,11 +1,16 @@
 const TodoNew = (props) => {
-  console.log(props);
   const { addNewTodo } = props;
   // addNewTodo();
+  const handleClick = () => {
+    alert("hello");
+  };
+  const handleOnChange = (event) => {
+    console.log(event.target.value);
+  };
   return (
     <div className="todo-new">
-      <input type="text" />
-      <button>Add</button>
+      <input type="text" onChange={handleOnChange} />
+      <button onClick={handleClick}>Add</button>
     </div>
   );
 };
