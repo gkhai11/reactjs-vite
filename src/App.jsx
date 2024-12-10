e0ac53706ab7790af24b23471eaccfae45a9e7b3
pgkhai <pgkhai1121@gmail.com>
2024-12-10 21:10:57 +0700

    'react-13'

--- src/App.jsx
+++ src/App.jsx
@@ -27,13 +27,19 @@ const App = () => {
   const randomIntFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
+
+  const deleteTodo = (id) => {
+    const newTodo = todoList.filter((item) => item.id !== id);
+    setTodoList(newTodo);
+  };
+
   return (
     <>
       <div className="todo-container">
         <div className="todo-title">Todo List</div>
         <TodoNew addNewTodo={addNewTodo} />
         {todoList.length > 0 ? (
-          <TodoData todoList={todoList} />
+          <TodoData todoList={todoList} deleteTodo={deleteTodo} />
         ) : (
           <div className="todo-image">
             <img src={reactLogo} className="logo" />
--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,6 +1,9 @@
 const TodoData = (props) => {
-  const { todoList } = props;
+  const { todoList, deleteTodo } = props;
   console.log("check", todoList);
+  const handleClick = (id) => {
+    deleteTodo(id);
+  };
   return (
     <div className="todo-data">
       {todoList.map((item, index) => {
@@ -8,7 +11,12 @@ const TodoData = (props) => {
           // eslint-disable-next-line react/jsx-key
           <div className="todo-item" key={item.id}>
             <div>{item.name}</div>
-            <button>Delete</button>
+            <button
+              onClick={() => handleClick(item.id)}
+              style={{ cursor: "pointer" }}
+            >
+              Delete
+            </button>
           </div>
         );
       })}
