58df148e5db16aea2738b3590e022a510c309105
pgkhai <pgkhai1121@gmail.com>
2025-01-08 12:28:30 +0700

    'react-23'

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,7 @@
-import TodoData from "./components/todo/TodoData";
-import TodoNew from "./components/todo/TodoNew";
-import "./components/todo/todo.css";
-import reactLogo from "./assets/react.svg";
-import { useState } from "react";
 import Header from "./components/layout/header.jsx";
 import Footer from "./components/layout/footer.jsx";
 import { Outlet } from "react-router-dom";
 
-const TodoApp = () => {
-  const [todoList, setTodoList] = useState([
-    // {
-    //   id: 1,
-    //   name: "Learn React",
-    // },
-    // {
-    //   id: 2,
-    //   name: "Learn Node",
-    // },
-  ]);
-
-  const addNewTodo = (name) => {
-    const newTodo = {
-      id: randomIntFromInterval(1, 999999),
-      name: name,
-    };
-    setTodoList([...todoList, newTodo]);
-  };
-
-  const randomIntFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  };
-
-  const deleteTodo = (id) => {
-    const newTodo = todoList.filter((item) => item.id !== id);
-    setTodoList(newTodo);
-  };
-  return (
-    <div className="todo-container">
-      <div className="todo-title">Todo List</div>
-      <TodoNew addNewTodo={addNewTodo} />
-      {todoList.length > 0 ? (
-        <TodoData todoList={todoList} deleteTodo={deleteTodo} />
-      ) : (
-        <div className="todo-image">
-          <img src={reactLogo} className="logo" />
-        </div>
-      )}
-    </div>
-  );
-};
 const App = () => {
   return (
     <>
