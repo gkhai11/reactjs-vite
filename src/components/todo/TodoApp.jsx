13bb2fe89f2919021f820c1a6c3b2d1859427f4c
pgkhai <pgkhai1121@gmail.com>
2025-01-08 12:18:21 +0700

    'react-21'

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,12 +1,8 @@
-import TodoData from "./components/todo/TodoData";
-import TodoNew from "./components/todo/TodoNew";
-import "./components/todo/todo.css";
-import reactLogo from "./assets/react.svg";
+import TodoData from "./TodoData";
+import TodoNew from "./TodoNew";
+import "./todo.css";
+import reactLogo from "../../assets/react.svg";
 import { useState } from "react";
-import Header from "./components/layout/header.jsx";
-import Footer from "./components/layout/footer.jsx";
-import { Outlet } from "react-router-dom";
-
 const TodoApp = () => {
   const [todoList, setTodoList] = useState([
     // {
@@ -49,14 +45,5 @@ const TodoApp = () => {
     </div>
   );
 };
-const App = () => {
-  return (
-    <>
-      <Header />
-      <Outlet />
-      <Footer />
-    </>
-  );
-};
 
-export default App;
+export default TodoApp;
