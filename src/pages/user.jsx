a686c77d62b432dd12fd9a1743ccb96d6a3ae692
pgkhai <pgkhai1121@gmail.com>
2025-01-16 15:56:12 +0700

    'base-user'

--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -1,5 +1,15 @@
+import UserTable from "../components/user/user.table";
+import UserForm from "../components/user/user.form";
+
 const UserPage = () => {
-  return <div>User Page</div>;
+  return (
+    <div>
+      <div style={{ padding: "20px" }}>
+        <UserForm />
+        <UserTable />
+      </div>
+    </div>
+  );
 };
 
 export default UserPage;
