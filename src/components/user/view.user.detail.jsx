46b6af6318f82dca64c6af794daad6517f2b102b
pgkhai <pgkhai1121@gmail.com>
2025-01-23 17:14:41 +0700

    'react-36'

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -1,8 +1,25 @@
 import { Drawer } from "antd";
 import { Button } from "antd";
+import { useState } from "react";
 
 const ViewUserDetail = (props) => {
   const { dataDetail, setDataDetail, isDetailOpen, setIsDetailOpen } = props;
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [preview, setPreview] = useState(null);
+
+  const handelOnChangeFile = (event) => {
+    if (!event.target.files || event.target.files.length === 0) {
+      setSelectedFile(null);
+      setPreview(null);
+      return;
+    }
+    const file = event.target.files[0];
+    if (file) {
+      setSelectedFile(file);
+      setPreview(URL.createObjectURL(file));
+    }
+  };
+  console.log("check file", preview);
   return (
     <Drawer
       width={"40vw"}
@@ -24,10 +41,16 @@ const ViewUserDetail = (props) => {
           <p>Phone number: {dataDetail.phone}</p>
           <br />
           <p>Avatar: </p>
-          <div>
+          <div
+            style={{
+              marginTop: "10px",
+              height: "150px",
+              width: "200px",
+              border: "1px solid #ccc",
+            }}
+          >
             <img
-              height={150}
-              width={200}
+              style={{ height: "100%", width: "100%", objectFit: "contain" }}
               src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${
                 dataDetail.avatar
               }`}
@@ -49,9 +72,30 @@ const ViewUserDetail = (props) => {
             >
               Upload Avatar
             </label>
-            <input type="file" hidden id="uploadBtn" />
+            <input
+              type="file"
+              hidden
+              id="uploadBtn"
+              onChange={(event) => {
+                handelOnChangeFile(event);
+              }}
+            />
           </div>
-          {/* <Button type="primary">Upload Avatar</Button> */}
+          {preview && (
+            <div
+              style={{
+                marginTop: "10px",
+                height: "150px",
+                width: "200px",
+                border: "1px solid #ccc",
+              }}
+            >
+              <img
+                style={{ height: "100%", width: "100%", objectFit: "contain" }}
+                src={preview}
+              />
+            </div>
+          )}
         </>
       ) : (
         <>
