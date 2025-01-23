247d70412cca2c5eccf31787b2898031560745b8
pgkhai <pgkhai1121@gmail.com>
2025-01-23 15:58:53 +0700

    'react-35'

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -1,9 +1,11 @@
 import { Drawer } from "antd";
+import { Button } from "antd";
 
 const ViewUserDetail = (props) => {
   const { dataDetail, setDataDetail, isDetailOpen, setIsDetailOpen } = props;
   return (
     <Drawer
+      width={"40vw"}
       title="Thong tin user"
       onClose={() => {
         setDataDetail(null);
@@ -21,6 +23,35 @@ const ViewUserDetail = (props) => {
           <br />
           <p>Phone number: {dataDetail.phone}</p>
           <br />
+          <p>Avatar: </p>
+          <div>
+            <img
+              height={150}
+              width={200}
+              src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${
+                dataDetail.avatar
+              }`}
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="uploadBtn"
+              style={{
+                display: "block",
+                width: "fit-content",
+                marginTop: "15px",
+                padding: "5px 10px",
+                background: "rgb(22 117 255)",
+                color: "white",
+                borderRadius: "5px",
+                cursor: "pointer",
+              }}
+            >
+              Upload Avatar
+            </label>
+            <input type="file" hidden id="uploadBtn" />
+          </div>
+          {/* <Button type="primary">Upload Avatar</Button> */}
         </>
       ) : (
         <>
