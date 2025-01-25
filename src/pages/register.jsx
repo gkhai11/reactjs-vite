775a83cf62d42353ec76aef166a09654eb1ca01d
pgkhai <pgkhai1121@gmail.com>
2025-01-26 03:20:51 +0700

    'react-40'

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,10 +1,31 @@
-import { Button, Input, Form } from "antd";
+import { Button, Input, Form, notification } from "antd";
+import { registerUserAPI } from "../services/api.service";
+import { useNavigate } from "react-router";
 
 const RegisterPage = () => {
+  let navigate = useNavigate();
   const [form] = Form.useForm();
 
-  const onFinish = (values) => {
+  const onFinish = async (values) => {
     console.log("check values", values);
+    const res = await registerUserAPI(
+      values.fullName,
+      values.email,
+      values.password,
+      values.phone
+    );
+    if (res.data) {
+      notification.success({
+        message: "Register User",
+        description: "Dang ky user thanh cong",
+      });
+      navigate("/login");
+    } else {
+      notification.error({
+        message: "Register User error",
+        description: JSON.stringify(res.message),
+      });
+    }
   };
   return (
     <Form
@@ -21,48 +42,49 @@ const RegisterPage = () => {
         <Form.Item
           label="Full Name"
           name="fullName"
-          // rules={[
-          //   {
-          //     required: true,
-          //     message: "Please input your username!",
-          //   },
-          // ]}
+          rules={[
+            {
+              required: true,
+              message: "Please input your full name!",
+            },
+          ]}
         >
           <Input />
         </Form.Item>
         <Form.Item
           label="Email"
           name="email"
-          // rules={[
-          //   {
-          //     required: true,
-          //     message: "Please input your username!",
-          //   },
-          // ]}
+          rules={[
+            {
+              required: true,
+              message: "Please input your email!",
+            },
+          ]}
         >
           <Input />
         </Form.Item>
         <Form.Item
           label="Password"
           name="password"
-          // rules={[
-          //   {
-          //     required: true,
-          //     message: "Please input your username!",
-          //   },
-          // ]}
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
         >
           <Input.Password />
         </Form.Item>
         <Form.Item
           label="Phone Number"
           name="phone"
-          // rules={[
-          //   {
-          //     required: true,
-          //     message: "Please input your username!",
-          //   },
-          // ]}
+          rules={[
+            {
+              required: true,
+              pattern: new RegExp(/\d+/g),
+              message: "Wrong format!",
+            },
+          ]}
         >
           <Input />
         </Form.Item>
