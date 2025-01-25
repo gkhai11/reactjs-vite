b3b2403f42fed481c0e3e58b9b1ca8f7c06bdee9
pgkhai <pgkhai1121@gmail.com>
2025-01-26 02:47:30 +0700

    'react-39'

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -31,8 +31,8 @@ const updateUserAvatarAPI = (avatar, _id, fullName, phone) => {
   return axios.put(URL_BACKEND, data);
 };
 
-const fetchAllUserAPI = () => {
-  const URL_BACKEND = "/api/v1/user";
+const fetchAllUserAPI = (current, pageSize) => {
+  const URL_BACKEND = `/api/v1/user?current=${current}&pageSize=${pageSize}`;
   return axios.get(URL_BACKEND);
 };
 
